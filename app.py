639c299bb4839e22f2affb9a46afdd4db51f14f8
Eetu Pylkkänen <eetu.pylkkanen@helsinki.fi>
2026-03-17 11:19:51 +0200

    Lisatty app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Tästä tulee työvuorojärjestelmä!"
+@app.route("/order")
+def order():
+    return render_template("order.html")
 
+@app.route("/result", methods=["POST"])
+def result():
+    pizza = request.form["pizza"]
+    extras = request.form.getlist("extra")
+    message = request.form["message"]
+    return render_template("result.html", pizza=pizza, extras=extras, message=message)
